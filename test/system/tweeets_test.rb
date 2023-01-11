d5d5c9c33d2841ee3985ce229ea8cb58c9c97aa1
ZikaZaki <zika.github@gmail.com>
2023-01-11 22:18:21 +0300

    Fix linter errors

--- test/system/tweeets_test.rb
+++ test/system/tweeets_test.rb
@@ -1,41 +1,41 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class TweeetsTest < ApplicationSystemTestCase
   setup do
     @tweeet = tweeets(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit tweeets_url
-    assert_selector "h1", text: "Tweeets"
+    assert_selector 'h1', text: 'Tweeets'
   end
 
-  test "should create tweeet" do
+  test 'should create tweeet' do
     visit tweeets_url
-    click_on "New tweeet"
+    click_on 'New tweeet'
 
-    fill_in "Tweeet", with: @tweeet.tweeet
-    click_on "Create Tweeet"
+    fill_in 'Tweeet', with: @tweeet.tweeet
+    click_on 'Create Tweeet'
 
-    assert_text "Tweeet was successfully created"
-    click_on "Back"
+    assert_text 'Tweeet was successfully created'
+    click_on 'Back'
   end
 
-  test "should update Tweeet" do
+  test 'should update Tweeet' do
     visit tweeet_url(@tweeet)
-    click_on "Edit this tweeet", match: :first
+    click_on 'Edit this tweeet', match: :first
 
-    fill_in "Tweeet", with: @tweeet.tweeet
-    click_on "Update Tweeet"
+    fill_in 'Tweeet', with: @tweeet.tweeet
+    click_on 'Update Tweeet'
 
-    assert_text "Tweeet was successfully updated"
-    click_on "Back"
+    assert_text 'Tweeet was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy Tweeet" do
+  test 'should destroy Tweeet' do
     visit tweeet_url(@tweeet)
-    click_on "Destroy this tweeet", match: :first
+    click_on 'Destroy this tweeet', match: :first
 
-    assert_text "Tweeet was successfully destroyed"
+    assert_text 'Tweeet was successfully destroyed'
   end
 end
